5b57f5c39a9a1c789bdb645293d70ccfc570e1c3
abhinavgitin <prabhudarsanpuri987@gmail.com>
2026-05-29 16:28:33 +0530

    fix: improve the invoice and pdf workflow

--- preload.js
+++ preload.js
@@ -1,6 +1,15 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
+    savePDF: (pdfData) => ipcRenderer.invoke('save-pdf', pdfData),
 
-    savePDF: (pdfData) => ipcRenderer.invoke('save-pdf', pdfData)
+    // Invoice DB APIs
+    saveInvoice: (invoiceData) => ipcRenderer.invoke('save-invoice', invoiceData),
+
+    loadInvoices: () => ipcRenderer.invoke('load-invoices'),
+
+    // Get next invoice number and clear invoices
+    getNextInvoiceNumber: () => ipcRenderer.invoke('get-next-invoice-number'),
+
+    clearInvoices: () => ipcRenderer.invoke('clear-invoices')
 });
